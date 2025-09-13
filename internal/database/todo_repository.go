e7b34d4ac447d94fa19a5c94e630872c8c55ac41
Umar Oladipo <oladipoumar@gmail.com>
2025-09-13 10:02:54 +0100

    remove committed database and IDE files

---
+++ internal/database/todo_repository.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	"github.com/horlerdipo/todo-golang/internal/enums"
+)
+
+type Todo struct {
+	Model
+	Title   string         `json:"title"`
+	Content string         `json:"content"`
+	Type    enums.TodoType `json:"type"`
+	UserID  uint           `json:"user_id"`
+	User    User           `gorm:"constraint:OnDelete:CASCADE" json:"user"`
+}
