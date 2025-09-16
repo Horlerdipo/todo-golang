ec2c05a1571d1061d720edc593236e3464387703
Umar Oladipo <oladipoumar@gmail.com>
2025-09-16 05:15:49 +0100

    unpin todo test made table driven

--- tests/integration/pin-todo_test.go
+++ tests/integration/pin-todo_test.go
@@ -2,14 +2,89 @@ package integration
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/horlerdipo/todo-golang/env"
 	"github.com/horlerdipo/todo-golang/internal/database"
+	"github.com/horlerdipo/todo-golang/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"testing"
 )
+
+func TestPinTodo(t *testing.T) {
+	//ARRANGE:
+	tests := []struct {
+		name               string
+		setupFunc          func(t *testing.T) PinTodoSetupResponse
+		expectedStatusCode int
+		expectedMsg        string
+		extraAssertions    func(t *testing.T, setupFuncResponse PinTodoSetupResponse)
+	}{
+		{
+			name:               "success",
+			setupFunc:          setupPinTodoSuccessfullyTest,
+			expectedStatusCode: http.StatusOK,
+			expectedMsg:        "",
+			extraAssertions:    assertTodoPinned,
+		},
+		{
+			name:               "unauthorized",
+			setupFunc:          setupPinTodoUnauthorizedTest,
+			expectedStatusCode: http.StatusUnauthorized,
+			expectedMsg:        "Unauthenticated",
+			extraAssertions:    assertPinTodoUnauthorized,
+		},
+		{
+			name:               "maximum pinned todo",
+			setupFunc:          setupPinTodoMaximumTest,
+			expectedStatusCode: http.StatusBadRequest,
+			expectedMsg:        "you can only pin " + env.FetchString("MAXIMUM_PINNED_TODOS") + " todos",
+			extraAssertions:    assertPinTodoMaximum,
+		},
+		{
+			name:               "todo not found",
+			setupFunc:          setupPinTodoNotFoundTest,
+			expectedStatusCode: http.StatusBadRequest,
+			expectedMsg:        "todo does not exist",
+			extraAssertions:    assertPinTodoNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setupResponse := tt.setupFunc(t)
+			url := fmt.Sprintf("%s/todos/%d/pin", TestServerInstance.Server.URL, setupResponse.Todo.ID)
+
+			req, err := http.NewRequest(http.MethodPatch, url, bytes.NewBuffer(nil))
+			require.NoError(t, err)
+
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("Authorization", "Bearer "+setupResponse.AuthToken)
+
+			//ACT
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			require.NoError(t, err)
+			defer resp.Body.Close()
+
+			//ASSERT
+			assert.Equal(t, tt.expectedStatusCode, resp.StatusCode)
+
+			if tt.expectedMsg != "" {
+				var response utils.JsonResponse[interface{}]
+				err := json.NewDecoder(resp.Body).Decode(&response)
+				require.NoError(t, err)
+				assert.Equal(t, tt.expectedMsg, response.Message)
+			}
+
+			if tt.extraAssertions != nil {
+				tt.extraAssertions(t, setupResponse)
+			}
+		})
+	}
+}
 
 func SetupPinTest(t *testing.T) (*database.User, string, *database.Todo) {
 	ClearAllTables(t, TestServerInstance.DB)
@@ -26,7 +101,6 @@ type PinTodoSetupResponse struct {
 }
 
 func setupPinTodoSuccessfullyTest(t *testing.T) PinTodoSetupResponse {
-	ClearAllTables(t, TestServerInstance.DB)
 	user, authToken, todo := SetupPinTest(t)
 	return PinTodoSetupResponse{
 		User:      user,
@@ -43,7 +117,6 @@ func assertTodoPinned(t *testing.T, setupFuncResponse PinTodoSetupResponse) {
 }
 
 func setupPinTodoUnauthorizedTest(t *testing.T) PinTodoSetupResponse {
-	ClearAllTables(t, TestServerInstance.DB)
 	user, _, todo := SetupPinTest(t)
 	return PinTodoSetupResponse{
 		User:      user,
@@ -60,7 +133,6 @@ func assertPinTodoUnauthorized(t *testing.T, setupFuncResponse PinTodoSetupRespo
 }
 
 func setupPinTodoMaximumTest(t *testing.T) PinTodoSetupResponse {
-	ClearAllTables(t, TestServerInstance.DB)
 	user, authToken, todo := SetupPinTest(t)
 
 	maximumPinnedTodo := env.FetchInt("MAXIMUM_PINNED_TODOS", 1)
@@ -85,7 +157,6 @@ func assertPinTodoMaximum(t *testing.T, setupFuncResponse PinTodoSetupResponse) 
 }
 
 func setupPinTodoNotFoundTest(t *testing.T) PinTodoSetupResponse {
-	ClearAllTables(t, TestServerInstance.DB)
 	user, authToken, todo := SetupPinTest(t)
 	todo.ID = todo.ID + 1
 	return PinTodoSetupResponse{
@@ -100,63 +171,4 @@ func assertPinTodoNotFound(t *testing.T, setupFuncResponse PinTodoSetupResponse)
 	result := TestServerInstance.DB.Where("id = ?", setupFuncResponse.Todo.ID-1).First(&newTodo)
 	assert.NoError(t, result.Error)
 	assert.Equal(t, false, newTodo.Pinned)
-}
-
-func TestPinTodo(t *testing.T) {
-	//ARRANGE:
-	tests := []struct {
-		name               string
-		setupFunc          func(t *testing.T) PinTodoSetupResponse
-		expectedStatusCode int
-		expectedMsg        string
-		extraAssertions    func(t *testing.T, setupFuncResponse PinTodoSetupResponse)
-	}{
-		{
-			name:               "success",
-			setupFunc:          setupPinTodoSuccessfullyTest,
-			expectedStatusCode: http.StatusOK,
-			extraAssertions:    assertTodoPinned,
-		},
-		{
-			name:               "unauthorized",
-			setupFunc:          setupPinTodoUnauthorizedTest,
-			expectedStatusCode: http.StatusUnauthorized,
-			extraAssertions:    assertPinTodoUnauthorized,
-		},
-		{
-			name:               "maximum pinned todo",
-			setupFunc:          setupPinTodoMaximumTest,
-			expectedStatusCode: http.StatusBadRequest,
-			extraAssertions:    assertPinTodoMaximum,
-		},
-		{
-			name:               "todo not found",
-			setupFunc:          setupPinTodoNotFoundTest,
-			expectedStatusCode: http.StatusBadRequest,
-			extraAssertions:    assertPinTodoNotFound,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			setupResponse := tt.setupFunc(t)
-			url := fmt.Sprintf("%s/todos/%d/pin", TestServerInstance.Server.URL, setupResponse.Todo.ID)
-
-			req, err := http.NewRequest(http.MethodPatch, url, bytes.NewBuffer(nil))
-			require.NoError(t, err)
-
-			req.Header.Set("Content-Type", "application/json")
-			req.Header.Set("Authorization", "Bearer "+setupResponse.AuthToken)
-
-			//ACT
-			client := &http.Client{}
-			resp, err := client.Do(req)
-			require.NoError(t, err)
-			defer resp.Body.Close()
-
-			//ASSERT
-			assert.Equal(t, tt.expectedStatusCode, resp.StatusCode)
-			tt.extraAssertions(t, setupResponse)
-		})
-	}
 }
--- tests/integration/unpin-todo_test.go
+++ tests/integration/unpin-todo_test.go
@@ -3,16 +3,92 @@ package integration
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/horlerdipo/todo-golang/internal/database"
 	"github.com/horlerdipo/todo-golang/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"io"
-	"io/ioutil"
 	"net/http"
-	"strconv"
 	"testing"
 )
+
+func TestUnpinTodo(t *testing.T) {
+	//ARRANGE:
+	tests := []struct {
+		name               string
+		setupFunc          func(t *testing.T) UnpinTodoSetupResponse
+		expectedStatusCode int
+		expectedMsg        string
+		extraAssertions    func(t *testing.T, setupResponse UnpinTodoSetupResponse)
+	}{
+		{
+			name:               "success",
+			setupFunc:          setupUnPinTodoSuccess,
+			expectedStatusCode: http.StatusOK,
+			expectedMsg:        "",
+			extraAssertions:    assertUnPinTodoSuccess,
+		},
+		{
+			name:               "not found",
+			setupFunc:          setupUnPinTodoNotFound,
+			expectedStatusCode: http.StatusBadRequest,
+			expectedMsg:        "todo does not exist",
+			extraAssertions:    nil,
+		},
+		{
+			name:               "unauthorized",
+			setupFunc:          setupUnPinTodoUnauthorized,
+			expectedStatusCode: http.StatusUnauthorized,
+			expectedMsg:        "Unauthenticated",
+			extraAssertions:    nil,
+		},
+		{
+			name:               "different user todo",
+			setupFunc:          setupUnPinTodoDifferentUser,
+			expectedStatusCode: http.StatusBadRequest,
+			expectedMsg:        "todo does not exist",
+			extraAssertions:    assertUnPinTodoDifferentUser,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setupResponse := tt.setupFunc(t)
+			url := fmt.Sprintf("%s/todos/%d/unpin", TestServerInstance.Server.URL, setupResponse.Todo.ID)
+
+			req, err := http.NewRequest(http.MethodPatch, url, bytes.NewBuffer(nil))
+			require.NoError(t, err)
+
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("Authorization", "Bearer "+setupResponse.AuthToken)
+
+			//ACT
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			require.NoError(t, err)
+			defer resp.Body.Close()
+
+			//ASSERT:
+			assert.Equal(t, tt.expectedStatusCode, resp.StatusCode)
+			if tt.expectedMsg != "" {
+				var response utils.JsonResponse[interface{}]
+				err := json.NewDecoder(resp.Body).Decode(&response)
+				require.NoError(t, err)
+				assert.Equal(t, tt.expectedMsg, response.Message)
+			}
+
+			if tt.extraAssertions != nil {
+				tt.extraAssertions(t, setupResponse)
+			}
+		})
+	}
+}
+
+type UnpinTodoSetupResponse struct {
+	User      *database.User
+	AuthToken string
+	Todo      *database.Todo
+}
 
 func SetupUnPinTest(t *testing.T) (*database.User, string, *database.Todo) {
 	ClearAllTables(t, TestServerInstance.DB)
@@ -22,82 +98,62 @@ func SetupUnPinTest(t *testing.T) (*database.User, string, *database.Todo) {
 	return user, authToken, todo
 }
 
-func TestUnPinTodo_Success(t *testing.T) {
-	//ARRANGE
-	_, authToken, todo := SetupPinTest(t)
+func setupUnPinTodoSuccess(t *testing.T) UnpinTodoSetupResponse {
+	user, authToken, todo := SetupUnPinTest(t)
+	return UnpinTodoSetupResponse{
+		User:      user,
+		AuthToken: authToken,
+		Todo:      todo,
+	}
+}
 
-	req, err := http.NewRequest(http.MethodPatch, TestServerInstance.Server.URL+"/todos/"+strconv.Itoa(int(todo.ID))+"/unpin", bytes.NewBuffer(nil))
-	require.NoError(t, err)
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+authToken)
-	//ACT
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	require.NoError(t, err)
-	defer resp.Body.Close()
-
-	//ASSERT
-	require.Equal(t, http.StatusOK, resp.StatusCode)
+func assertUnPinTodoSuccess(t *testing.T, setupResponse UnpinTodoSetupResponse) {
 	newTodo := &database.Todo{}
-	result := TestServerInstance.DB.Where("id = ?", todo.ID).First(&newTodo)
+	result := TestServerInstance.DB.Where("id = ?", setupResponse.Todo.ID).First(&newTodo)
 	assert.NoError(t, result.Error)
 	assert.Equal(t, false, newTodo.Pinned)
 }
 
-func TestUnPinTodo_NotFoundError(t *testing.T) {
-	//ARRANGE
-	ClearAllTables(t, TestServerInstance.DB)
-	_, authToken, todo := SetupPinTest(t)
-
-	req, err := http.NewRequest(http.MethodPatch, TestServerInstance.Server.URL+"/todos/"+strconv.Itoa(int(todo.ID-1))+"/unpin", bytes.NewBuffer(nil))
-	require.NoError(t, err)
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+authToken)
-
-	//ACT
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	require.NoError(t, err)
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	require.NoError(t, err)
-
-	var responseJson utils.JsonResponse[interface{}]
-	err = json.Unmarshal(body, &responseJson)
-	require.NoError(t, err)
-
-	//ASSERT
-	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-	assert.Equal(t, responseJson.Message, "todo does not exist")
+func setupUnPinTodoUnauthorized(t *testing.T) UnpinTodoSetupResponse {
+	user, _, todo := SetupUnPinTest(t)
+	return UnpinTodoSetupResponse{
+		User:      user,
+		AuthToken: "incorrect-auth-token",
+		Todo:      todo,
+	}
 }
 
-func TestUnPinTodo_UnauthorizedError(t *testing.T) {
-	//ARRANGE
-	ClearAllTables(t, TestServerInstance.DB)
-	_, _, todo := SetupPinTest(t)
+func setupUnPinTodoNotFound(t *testing.T) UnpinTodoSetupResponse {
+	user, authToken, todo := SetupUnPinTest(t)
+	todo.ID = todo.ID + 1
+	return UnpinTodoSetupResponse{
+		User:      user,
+		AuthToken: authToken,
+		Todo:      todo,
+	}
+}
 
-	req, err := http.NewRequest(http.MethodPatch, TestServerInstance.Server.URL+"/todos/"+strconv.Itoa(int(todo.ID-1))+"/unpin", bytes.NewBuffer(nil))
-	require.NoError(t, err)
+func setupUnPinTodoDifferentUser(t *testing.T) UnpinTodoSetupResponse {
+	user, _, todo := SetupUnPinTest(t)
+	anotherUser := &database.User{}
 
-	req.Header.Set("Content-Type", "application/json")
+	anotherUser = SeedUser(t, struct {
+		Email string
+	}{
+		Email: "user2@gmail.com",
+	})
+	authToken := GenerateTestJwtToken(t, anotherUser.ID)
 
-	//ACT
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	require.NoError(t, err)
-	defer resp.Body.Close()
+	return UnpinTodoSetupResponse{
+		User:      user,
+		AuthToken: authToken,
+		Todo:      todo,
+	}
+}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	require.NoError(t, err)
-
-	var responseJson utils.JsonResponse[interface{}]
-	err = json.Unmarshal(body, &responseJson)
-	require.NoError(t, err)
-
-	//ASSERT
-	assert.Equal(t, http.StatusUnauthorized, resp.StatusCode)
-	assert.Equal(t, responseJson.Message, "Unauthenticated")
+func assertUnPinTodoDifferentUser(t *testing.T, setupResponse UnpinTodoSetupResponse) {
+	newTodo := &database.Todo{}
+	result := TestServerInstance.DB.Where("id = ?", setupResponse.Todo.ID).First(&newTodo)
+	assert.NoError(t, result.Error)
+	assert.Equal(t, true, newTodo.Pinned)
 }
